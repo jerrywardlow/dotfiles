08d5815b70d1f02d1bcb572b9c6333f6f9bcb489
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-01 13:46:41 -0700

    Remove SSH items

--- install.sh
+++ install.sh
@@ -9,11 +9,6 @@ do
     ln -snf $DIR/$item ~
 done
 
-curl https://github.com/jerrywardlow.keys >> ~/.ssh/authorized_keys
-
-chmod 0700 ~/.ssh
-chmod 0600 ~/.ssh/authorized_keys
-
 vim +PlugInstall +qall
 
 source ~/.bashrc
