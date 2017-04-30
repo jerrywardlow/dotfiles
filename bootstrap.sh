54f912145b2f1785a2738c0b8e83613f6460e343
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-30 16:53:24 -0700

    Clone dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,6 +22,7 @@ then
     PASSWORD='$6$rounds=666666$hypertension$LxNM/PPbjauIBoXp0XPgOdYmr/jXvPwei133CZfbUxWoPR6U.JLAKyCFCln33ikGUWIQBEOPVCvLm.7hoHT.f1'
 fi
 
-id -u $USER &>/dev/null || useradd -G sudo -p $PASS $USER
-
+id -u $USER &>/dev/null || useradd -G sudo -p $PASSWORD $USER
 curl https://github.com/jerrywardlow.keys >> /home/$USER/.ssh/authorized_keys
+su $USER -c "git clone https://github.com/jerrywardlow/dotfiles /home/$USER/dotfiles/"
+su $USER -c "/home/$USER/dotfiles/install.sh"
