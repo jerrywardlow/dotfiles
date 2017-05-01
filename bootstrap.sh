7a9a817538f144d9f6012b6a6fa54743a1d0afb4
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-30 19:45:53 -0700

    Update variables

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-set -eu
+set -e
 
 # Dependencies
 
@@ -12,9 +12,9 @@ fi
 
 # User
 
-if [ -z $USER ]
+if [ -z $NEWUSER ]
 then
-    USER=jerry
+    NEWUSER=jerry
 fi
 
 if [ -z $PASSWORD ]
@@ -22,14 +22,14 @@ then
     PASSWORD='$6$rounds=666666$hypertension$LxNM/PPbjauIBoXp0XPgOdYmr/jXvPwei133CZfbUxWoPR6U.JLAKyCFCln33ikGUWIQBEOPVCvLm.7hoHT.f1'
 fi
 
-id -u $USER &>/dev/null || useradd -G sudo -p $PASSWORD $USER
-curl https://github.com/jerrywardlow.keys >> /home/$USER/.ssh/authorized_keys
-su $USER -c "git clone https://github.com/jerrywardlow/dotfiles /home/$USER/dotfiles/"
-su $USER -c "/home/$USER/dotfiles/install.sh"
+id -u $NEWUSER &>/dev/null || useradd -G sudo -p $PASSWORD $NEWUSER
+curl https://github.com/jerrywardlow.keys >> /home/$NEWUSER/.ssh/authorized_keys
+su $NEWUSER -c "git clone https://github.com/jerrywardlow/dotfiles /home/$NEWUSER/dotfiles/"
+su $NEWUSER -c "/home/$NEWUSER/dotfiles/install.sh"
 
 # SSH
 
 sed -i -e '/^PermitRootLogin/s/^.*$/PermitRootLogin no/' /etc/ssh/sshd_config
 sed -i -e '/^PasswordAuthentication/s/^.*$/PasswordAuthentication no/' /etc/ssh/sshd_config
-echo "\nAllowUsers $USER" >> /etc/ssh/sshd_config
+echo "\nAllowUsers $NEWUSER" >> /etc/ssh/sshd_config
 service ssh restart
