795eb37cbd6bd7aa02277e717f86349609f40475
Jerry Wardlow <jerry@jerryoftheyear.com>
2018-02-27 17:07:37 -0800

    Clean up this garbage script

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,13 +12,11 @@ fi
 
 # User
 
-if [ -z $NEWUSER ]
-then
+if [ -z $NEWUSER ]; then
     NEWUSER=jerry
 fi
 
-if [ -z $PASSWORD ]
-then
+if [ -z $PASSWORD ]; then
     PASSWORD='$6$rounds=666666$hypertension$LxNM/PPbjauIBoXp0XPgOdYmr/jXvPwei133CZfbUxWoPR6U.JLAKyCFCln33ikGUWIQBEOPVCvLm.7hoHT.f1'
 fi
 
