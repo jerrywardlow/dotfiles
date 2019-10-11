e45336cf83e04e67d33b27c6eed52c2975796c0c
Jerry Wardlow <jerry@jerryoftheyear.com>
2019-10-11 11:31:38 -0700

    Update install script to pull most recent version of vim-plug

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
 
+# Setup Vim plugin directory
+mkdir -p ~/.vim/autoload/
+
+# Pull most recent version of vim-plug plugin manager
+curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > ~/.vim/autoload/plug.vim
+
 for item in .conf.d .bashrc .gitconfig .tmux.conf .vimrc .vim
 do
     ln -snf $DIR/$item ~
